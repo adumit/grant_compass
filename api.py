12c44dd4ae15f3e998f58deb7be5fb86839bac4b
Andrew Dumit <andrew.dumit@gmail.com>
2023-09-30 13:35:45 -0600

    Fix search and API return to get the actual grant information back (#6)

--- api.py
+++ api.py
@@ -1,6 +1,12 @@
+import orjson
+
 from fastapi import FastAPI
 
+from embed import get_embedding
+from search_text import search_embeddings
+
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
@@ -9,4 +15,16 @@ async def root():
 
 @app.get("/search/")
 async def get_item(search_text: str):
-    return {"search_text": search_text}
+    with open("data/test_grants.json") as f:
+        grant_embeddings = orjson.loads(f.read())
+
+    top_indices = search_embeddings(
+        search_text, [g["embedding"] for g in grant_embeddings], top_n=10
+    )
+    print(top_indices)
+    return {
+        "Top matches": [
+            {k: v for k, v in grant_embeddings[i].items() if k != "embedding"}
+            for i in top_indices
+        ]
+    }
