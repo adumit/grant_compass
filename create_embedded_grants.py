b788693b7934c0c71c82b44e0559fa25bfa540f5
Andrew Dumit <andrew.dumit@gmail.com>
2023-10-07 16:58:20 -0600

    Fix embedding script to work with all current grants (#9)

--- create_embedded_grants.py
+++ create_embedded_grants.py
@@ -12,6 +12,10 @@ from embed import get_embedding
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
+def convert_date(date_str: str):
+    return f"{date_str[-4:]}-{date_str[:2]}-{date_str[2:4]}"
+
+
 def create_grant_test_data(n_grants: int = 50):
     print("Reading in raw data...")
     grants_data = xmltodict.parse(open("./data/GrantsDBExtract20230926v2.xml").read())
@@ -23,24 +27,34 @@ def create_grant_test_data(n_grants: int = 50):
             x
             for x in grants_data["Grants"]["OpportunitySynopsisDetail_1_0"]
             if "CloseDate" in x
-            and x["CloseDate"][-4:] >= f"{today.month:02}{today.day:02}{today.year}"
+            and convert_date(x["CloseDate"])
+            >= f"{today.year}-{today.month:02}-{today.day:02}"
         ],
         key=lambda x: x["CloseDate"],
         reverse=True,
     )
+
+    if n_grants == -1:
+        n_grants = len(current_grants)
+    fname = f"./data/test_grants_n={n_grants}.json"
     small_batch = current_grants[:n_grants]
-    embeddings = get_embedding([x["Description"] for x in small_batch])
+    print(f"Embedding {len(small_batch)} documents")
+    embeddings = get_embedding(
+        [x["Description"] for x in small_batch if "Description" in x]
+    )
 
     for i, item in enumerate(small_batch):
         item["embedding"] = embeddings[i]
 
-    os.makedirs("./test_data", exist_ok=True)
-    with open("./test_data/test_grants.json", "wb") as f:
+    os.makedirs("./data", exist_ok=True)
+    with open(fname, "wb") as f:
         f.write(orjson.dumps(small_batch))
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Create a small amount of test data")
+    parser = argparse.ArgumentParser(
+        description="Embed N currently open grants from hard-coded xml file (-1 for all grants)"
+    )
     parser.add_argument(
         "--num_grants",
         type=int,
