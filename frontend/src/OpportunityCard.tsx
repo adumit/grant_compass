b387992cf341efe38014e555fa4b91f8f07f45b5
Andrew Dumit <andrew.dumit@buoyhealth.com>
2023-11-04 18:10:41 -0600

    Add opportunity URL to title

--- frontend/src/OpportunityCard.tsx
+++ frontend/src/OpportunityCard.tsx
@@ -5,10 +5,15 @@ export interface OpportunityCardProps {
 }
 
 export default function OpportunityCard({opportunity}: OpportunityCardProps) {
+  const opportunityUrl = `https://grants.gov/search-results-detail/${opportunity.OpportunityID}`;
 
   return (
     <li key={opportunity.OpportunityID}>
-      <h2>{opportunity.OpportunityTitle}</h2>
+      <h2>
+        <a href={opportunityUrl} target="_blank" rel="noopener noreferrer">
+          {opportunity.OpportunityTitle}
+        </a>
+      </h2>
       <h3>{opportunity.Description}</h3>
     </li>
   );
