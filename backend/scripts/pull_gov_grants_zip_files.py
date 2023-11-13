f0de53eb3caee4b124cef2654f3ca96e82d34069
Andrew Dumit <andrew.dumit@buoyhealth.com>
2023-11-12 17:28:02 -0700

    Move script around; add main callability

--- backend/scripts/pull_gov_grants_zip_files.py
+++ backend/scripts/pull_gov_grants_zip_files.py
@@ -1,20 +1,16 @@
+import argparse
 import os
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
+from ..create_embedded_grants import get_current_grants
 import time
-
-from ..backend.create_embedded_grants import get_current_grants
-
-# TODO: Make this enterable via command line
-DOWNLOAD_PATH = ""
 
 
 def all_downloads_complete(driver):  # Driver is unused, but automatically passed in
@@ -23,10 +19,9 @@ def all_downloads_complete(driver):  # Driver is unused, but automatically passe
 
 
 def download_single_grant(opportunity_id: int):
+    print("STARTING SINGLE GRANT DOWNLOAD")
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-    # driver = webdriver.Firefox()
-    oppy_id = "350283"
-    driver.get(f"https://www.grants.gov/search-results-detail/{oppy_id}")
+    driver.get(f"https://www.grants.gov/search-results-detail/{opportunity_id}")
 
     button_text = "Related Documents"
     button = WebDriverWait(driver, 10).until(
@@ -57,3 +52,15 @@ def pull_gov_grants_zip_files():
     current_opportunity_ids = [x["OpportunityID"] for x in current_grants]
     for grant_opp in current_opportunity_ids[:10]:
         download_single_grant(grant_opp)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Download zip files for current grants"
+    )
+    parser.add_argument(
+        "--download_path", type=str, help="Path to check for complete downloads"
+    )
+    args = parser.parse_args()
+    DOWNLOAD_PATH = args.download_path
+    pull_gov_grants_zip_files()
