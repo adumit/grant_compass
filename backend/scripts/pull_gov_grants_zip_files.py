0e971edf898e512900f61d952ecc65ca756d802c
Andrew Dumit <andrew.dumit@buoyhealth.com>
2023-11-12 17:43:35 -0700

    Save relationship between opportunity ID and file names

--- backend/scripts/pull_gov_grants_zip_files.py
+++ backend/scripts/pull_gov_grants_zip_files.py
@@ -1,7 +1,9 @@
 import argparse
 import os
+import json
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -13,13 +15,23 @@ from ..create_embedded_grants import get_current_grants
 import time
 
 
+"""
+
+To run this script, run: 
+python3 -m backend.scripts.pull_gov_grants_zip_files --download_path <PATH TO YOUR DOWNLOAD FOLDER>
+This should be run from the root of the project to download locally.
+Note: the method for checking for complete downloads likely does not work on windows
+
+"""
+DOWNLOAD_PATH = ""  # This should be changed via running this as main.
+
+
 def all_downloads_complete(driver):  # Driver is unused, but automatically passed in
     downloads = os.listdir(DOWNLOAD_PATH)
     return not any([x.endswith(".crdownload") for x in downloads])
 
 
-def download_single_grant(opportunity_id: int):
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+def download_single_grant(driver, opportunity_id: int) -> list[str]:
     driver.get(f"https://www.grants.gov/search-results-detail/{opportunity_id}")
 
     button_text = "Related Documents"
@@ -35,6 +47,7 @@ def download_single_grant(opportunity_id: int):
 
     # Filter for .zip file links
     zip_links = [l for l in links if ".zip" in l.text]
+    files_before = os.listdir(DOWNLOAD_PATH)
 
     for l in zip_links:
         l.click()
@@ -43,16 +56,30 @@ def download_single_grant(opportunity_id: int):
 
     # waits for all the files to be completed and returns the paths
     WebDriverWait(driver, 120, 1).until(all_downloads_complete)
-    driver.quit()
+    files_after = os.listdir(DOWNLOAD_PATH)
+    new_files = [f for f in files_after if f not in files_before]
+    return new_files
 
 
-def pull_gov_grants_zip_files():
+def pull_gov_grants_zip_files() -> dict[int, list[str]]:
+    chrome_options = Options()
+    prefs = {"download.default_directory": DOWNLOAD_PATH}
+    chrome_options.add_experimental_option("prefs", prefs)
+
+    driver = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()), options=chrome_options
+    )
+
+    grant_opportunity_to_fnames = {}
     current_grants = get_current_grants()
     current_opportunity_ids = [x["OpportunityID"] for x in current_grants]
     for i, grant_opp in enumerate(current_opportunity_ids):
         if i % 100 == 0:
             print(f"Downloading grant {i+1} of {len(current_opportunity_ids)}")
-        download_single_grant(grant_opp)
+        downloaded_files = download_single_grant(driver, grant_opp)
+        grant_opportunity_to_fnames[grant_opp] = downloaded_files
+    driver.quit()
+    return grant_opportunity_to_fnames
 
 
 if __name__ == "__main__":
@@ -64,4 +91,9 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
     DOWNLOAD_PATH = args.download_path
-    pull_gov_grants_zip_files()
+    opportunity_id_to_files = pull_gov_grants_zip_files()
+    with open(
+        os.path.join(os.path.dirname(__file__), "grant_opportunity_to_fnames.json"),
+        "w",
+    ) as f:
+        json.dump(str(opportunity_id_to_files))
